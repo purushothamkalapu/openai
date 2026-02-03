79db78de2e065b333d263c0e509aa37c664befbd
Purushotham <119475738+purushothamkalapu@users.noreply.github.com>
2026-02-03 21:34:57 +0530

    Initial commit

---
+++ src/main/java/com/eazybyte/openai/controller/ChatController.java
@@ -0,0 +1,47 @@
+package com.eazybyte.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+    /*
+     This is related to Open AI
+
+    private final ChatClient chatClient;
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+    @GetMapping("/chat")
+    public String chatUsingOpenAI(@RequestParam String message) {
+        return chatClient.prompt(message)
+                .call()
+                .content();
+    }*/
+    private final OpenAiChatModel chatModel;
+
+    public ChatController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+}
